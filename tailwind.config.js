7e2c0490c8f84c7915bba1a70694b8902790648d
Vishvjeet-Rana <aryanrana62366@gmail.com>
2024-12-17 17:11:04 +0530

    added colors and font family in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        mullish: ["Mullish", "sans-serif"],
+      },
+      colors: {
+        deepBlue: "#001f54",
+        lightBlue: "#add8e6",
+        lightBlue300: "#4fc3f7",
+        lightBlue500: "#03a9f4",
+        greeLight: "#8bc34a",
+        gratText: "#9e9e9e",
+        lightGray: "#d3d3d3",
+        grayBlue: "#5a647e",
+        deepBlueHead: "#0d47a1",
+        gray2: "#525a76",
+      },
+    },
   },
   plugins: [],
 };
