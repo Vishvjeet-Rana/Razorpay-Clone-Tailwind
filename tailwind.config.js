593ad075b051a049d0fd35422a161792786e50f5
Vishvjeet-Rana <aryanrana62366@gmail.com>
2024-12-18 17:29:11 +0530

    hero section with animation completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,31 @@ module.exports = {
   content: ["*"],
   theme: {
     extend: {
+      animation: {
+        "slide-in-left": "slideInLeft 1s ease-out",
+        "slide-in-right": "slideInRight 1s ease-out",
+        "slide-in-top": "slideInTop 1s ease-out",
+        "slide-in-bottom": "slideInBottom 1s ease-out",
+      },
+      keyframes: {
+        slideInLeft: {
+          "0%": { transform: "translateX(-100%)", opacity: "0" },
+          "100%": { transform: "translateX(0)", opacity: "1" },
+        },
+        slideInRight: {
+          "0%": { transform: "translateX(100%)", opacity: "0" },
+          "100%": { transform: "translateX(0)", opacity: "1" },
+        },
+        slideInTop: {
+          "0%": { transform: "translateY(-100%)", opacity: "0" },
+          "100%": { transform: "translateY(0)", opacity: "1" },
+        },
+        slideInBottom: {
+          "0%": { transform: "translateY(100%)", opacity: "0" },
+          "100%": { transform: "translateY(0)", opacity: "1" },
+        },
+      },
+
       fontFamily: {
         mullish: ["Mullish", "sans-serif"],
       },
@@ -18,6 +43,8 @@ module.exports = {
         deepBlueHead: "#0d47a1",
         gray2: "#525a76",
         darkGray: "#A9A9A9",
+        WhiteBlue: "#D9D9FF",
+        BtnClr: "#7B99FF",
       },
     },
   },
